c3f92e3abc28c66fc9b7a89ed4b5d7a339c8ab40
Devanada <aqil@intermediatech.id>
2022-02-24 12:47:10 +0700

    pertemuan ke 2 edited

--- Modul2/web-react-ku/src/index.js
+++ Modul2/web-react-ku/src/index.js
@@ -8,19 +8,16 @@ import './App.css';
 const Hello = () => {
   return [
   <div className="App">,
-  <header className="App-header">,
-        <img src={logo} className="App-logo" alt="logo" />,
-      <marquee behavior="scroll" direction="up" scrollamount="7">Bangun Tidur</marquee>,
-      <marquee behavior="scroll" direction="right" scrollamount="12">Kuliah</marquee>,
-      <marquee behavior="scroll" direction="left" scrollamount="10">Ngoding & learning</marquee>,
-      <marquee behavior="scroll" direction="right" >Isoman : istirahat solat makan</marquee>,
-      </header>,
+  <header className="App-header">
+      <p align="center">Hallo Dunia</p>
+      <img src={logo} className="App-logo" alt="logo" />
+      <marquee behavior="scroll" direction="up" scrollamount="7">Bangun Tidur</marquee>
+      <marquee behavior="scroll" direction="right" scrollamount="12">Kuliah</marquee>
+      <marquee behavior="scroll" direction="left" scrollamount="10">Ngoding & learning Working</marquee>
+      <marquee behavior="scroll" direction="right" >Isoman : istirahat solat makan</marquee>
+      </header>
   </div>
   ]
 }
-
 ReactDOM.render(<Hello />, document.getElementById('root'));
-//If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
