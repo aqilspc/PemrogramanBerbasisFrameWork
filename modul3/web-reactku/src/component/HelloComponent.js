7c126a64c228f341caa128fc32873185eb644140
unknown <aqil@intermediatech.id>
2022-03-10 15:04:00 +0700

    revert modul3

--- modul3/web-reactku/src/component/HelloComponent.js
+++ modul3/web-reactku/src/component/HelloComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 import './HelloComponent.css';
 
 const HelloComponent = () =>{
-    return <p className='test-p'> Saya adalah Dimas</p>
+    return <p className='test-p'> Saya adalah Aqil</p>
 }
 
 export default HelloComponent;
