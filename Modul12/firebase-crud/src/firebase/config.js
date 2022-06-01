d023c3c01edc458df4849b3e10ede9bd3e5f3d4a
unknown <aqil@intermediatech.id>
2022-06-01 07:09:58 +0700

    modul 12

---
+++ Modul12/firebase-crud/src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDwF9aYOQdeL78lFm5IkFSKPn8fn0_EorQ",
+  authDomain: "cobalogin-36072.firebaseapp.com",
+  databaseURL: "https://cobalogin-36072-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cobalogin-36072",
+  storageBucket: "cobalogin-36072.appspot.com",
+  messagingSenderId: "588711876058",
+  appId: "1:588711876058:web:8b559f1ff81ad40b779ce3"
+};
+export default firebaseConfig;
